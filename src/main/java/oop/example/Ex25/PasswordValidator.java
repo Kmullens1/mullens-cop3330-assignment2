64cb9ad037d9c0fed3eb3a259ce80d976952ad00
kmullens1 <kmullens1@knights.ucf.edu>
2021-07-27 00:12:23 -0400

    Exercises 24 & 25 complete - without testcases

--- src/main/java/oop/example/Ex25/PasswordValidator.java
+++ src/main/java/oop/example/Ex25/PasswordValidator.java
@@ -13,36 +13,97 @@ public class PasswordValidator {
         return password;
     }
 
-    boolean checkStrength(String password)
+    String checkStrength(String password)
     {
         String outputStatement = "";
-
+        int charTypeIndicator = 0;
         if(password.length() < 8)
         {
-
-            for(char c : password.toCharArray())
-            {
-                if (!Character.isDigit(c))
-                {
-                    return false;
-                }
-                else
-                    return true;
-            }
+            if(letters(password))
+                outputStatement = "The password '" + password + "' is a weak password.";
+            else
+                outputStatement = "The password '" + password + "' is a very weak password.";
 
         }
 
         else
         {
+            int counter = 0;
+            if(numbers(password))
+                counter = counter + 1;
 
+            if(letters(password))
+                counter = counter + 1;
+
+            if(special(password))
+                counter = counter + 1;
+
+            if(counter == 1)
+                outputStatement = "The password '" + password + "' is a weak password.";
+            if(counter == 2)
+                outputStatement = "The password '" + password + "' is a strong password.";
+            if(counter == 3)
+                outputStatement = "The password '" + password + "' is a very strong password.";
         }
 
-        // System.out.print("The password '" + password + "' is a very weak password.");
-        // System.out.print("The password '" + password + "' is a weak password.");
-        // System.out.print("The password '" + password + "' is a strong password.");
-        // System.out.print("The password '" + password + "' is a very strong password.");
-
         return outputStatement;
+    }
+
+    boolean letters(String password)
+    {
+        boolean indicator = false;
+        int counter = 0;
+        for(char c : password.toCharArray())
+        {
+            if (Character.isLetter(c))
+            {
+                counter = counter + 1;
+            }
+        }
+
+        if(counter >= 1)
+            indicator = true;
+
+        return indicator;
+    }
+
+
+    boolean numbers(String password)
+    {
+        boolean indicator = false;
+        int counter = 0;
+        for(char c : password.toCharArray())
+        {
+            if (Character.isDigit(c))
+            {
+                counter = counter + 1;
+            }
+        }
+
+        if(counter >= 1)
+            indicator = true;
+
+        return indicator;
+    }
+
+    boolean special(String password)
+    {
+        boolean indicator = false;
+        String specialChars[] = {" ", "~", "`", "!", "@", "#", "$", "%", "^",
+                "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]",
+                "|", "/", ":", ";", "'", ",", ".", "<", ">", "?", "\\", "\""};
+
+        int counter = 0;
+        for (int i=0; i < password.length(); i++)
+        {
+            if (password.contains(specialChars[i]))
+            {
+                counter = counter + 1;
+            }
+        }
+        if(counter >= 1)
+            indicator = true;
+        return indicator;
     }
 
 }
