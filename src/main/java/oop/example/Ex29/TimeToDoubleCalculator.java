c002bb31ae9c521eeac2e87635e10e10a7fa6105
kmullens1 <kmullens1@knights.ucf.edu>
2021-07-30 02:45:49 -0400

    Exercise 30 Complete plus Testcases

---
+++ src/main/java/oop/example/Ex29/TimeToDoubleCalculator.java
@@ -0,0 +1,23 @@
+package oop.example.Ex29;
+
+import java.util.Scanner;
+
+public class TimeToDoubleCalculator {
+
+    double timeToDouble()
+    {
+        String data = inputReader();
+        if(data == "0" || data.isDigit)
+    }
+
+    String inputReader()
+    {
+        Scanner input = new Scanner(System.in);
+
+        String data = input.nextLine();
+
+        return data;
+    }
+}
+
+
