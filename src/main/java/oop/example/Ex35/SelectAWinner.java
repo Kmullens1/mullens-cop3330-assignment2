eae1c14a404579b1d90369ba66d5dd9d938196ec
kmullens1 <kmullens1@knights.ucf.edu>
2021-08-01 18:23:49 -0400

    Exercise 35 Complete minus Testcases

--- src/main/java/oop/example/Ex35/SelectAWinner.java
+++ src/main/java/oop/example/Ex35/SelectAWinner.java
@@ -8,7 +8,7 @@ public class SelectAWinner {
 
     public String randomWinner(List names) {
         Random rand = new Random();
-        int randNum = rand.nextInt(names.size()); //FIXME - double check that I don't need to +1
+        int randNum = rand.nextInt(names.size());
 
         String winner = (String) names.get(randNum);
         return winner;
